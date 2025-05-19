147848ed62c454f98f75ab38780aaaa221dde368
yousef samir <yosifsamir454@gmail.com>
2025-05-20 02:17:42 +0300

    merge user rate data

--- neuro-fuzzy-model/src/movie_recommender/data/__init__.py
+++ neuro-fuzzy-model/src/movie_recommender/data/__init__.py
@@ -1,1 +1,18 @@
+"""
+Movie recommender data processing package.
+
+This package contains functions for processing and merging movie recommendation data,
+including ratings, tags, and other metadata.
+"""
+
 # Data processing module initialization
+
+
+from .merge_user_rate import (
+    merge_ratings_movies,
+    merge_with_links
+)
+
+
+# Add version info
+__version__ = '0.1.0'
